d4ef69cceac23339e43d7d00166d629e3f23cddc
Kwane Smith <smithkwane6@gmail.com>
2020-02-21 03:27:45 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,6 +6,15 @@ class ChattyStudent < Student
   end
 
   def raise_hand
-    10.times {super}
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
   end
 end
